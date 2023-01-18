f4513ca204369e1ddd8165d84ef5e1412f1762db
aj124578 <aj124578@naver.com>
2023-01-18 10:18:48 +0900

    mapper 쿼리 user_tb, product_tb 완료

--- src/main/java/shop/mtcoding/buyer/model/UserRepository.java
+++ src/main/java/shop/mtcoding/buyer/model/UserRepository.java
@@ -7,9 +7,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserRepository { // 기능을 모를때 CRUD
-    public int insert(String username, String password, String email);
-    public List<User> findAll();
     public User findById(int id); // id에 의해서 찾겠다 findOne으로 적으면 헷갈림
+    public List<User> findAll();
+    public int insert(String username, String password, String email);
     public int updateById(int id, String password); // id로 찾아서 업데이트 하겠다.
     public int deleteById(int id); // id로 삭제를 하겠다.
 
